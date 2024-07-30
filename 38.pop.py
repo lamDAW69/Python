5f360d5629d3d41726b16c4538f6faaa6a887368
lamDAW69 <118375353+lamDAW69@users.noreply.github.com>
2024-07-30 16:13:45 +0200

    Estructura con Poo

--- 38.pop.py
+++ 38.pop.py
@@ -10,18 +10,19 @@
 # Finalmente compara si 2 personas con los mismos atributos son iguales
 
 # Definición de la clase Persona
+
 class Persona:
-    def __init__(self, nombre, edad): 
+    def __init__(self, nombre, edad):       # Constructor	
         self.nombre = nombre
         self.edad = edad
     
     def __str__(self):                          # Representar como str: Pepito - 25
         return f"{self.nombre} - {self.edad}"
     
-    def __lt__(self, other):
+    def __lt__(self, other):                        # Comparar con < (por edad)
         return self.edad < other.edad
     
-    def __eq__(self, other):
+    def __eq__(self, other):                            # Comparar con == (por nombre y edad)	
         return self.nombre == other.nombre and self.edad == other.edad
 
 personas = [Persona("Pepito", 25), Persona("Juanito", 20), Persona("Maria", 30)]
